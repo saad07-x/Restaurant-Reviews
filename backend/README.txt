714506926bcc08efe06234c82dfd1c26c938abf0
saad07-x <mirsaad11@hotmail.com>
2021-06-30 22:19:29 +0500

    connected to server

---
+++ backend/README.txt
@@ -0,0 +1,1 @@
+In package.json added:  "type": "module",
---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+//var app = require("./server.js");
+
+console.log(mongodb);
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500, //in MS
+        useNewUrlParse: true
+    })
+
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log('listening on port '+port)
+        })
+    })
